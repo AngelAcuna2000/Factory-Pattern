f870772d3acac299754cd035eae6dc508f73a02d
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-30 13:44:51 -0400

    Exercise Complete

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace FactoryPattern
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many wheels does the vehicle you want to create have?");
+            string vehicleType = Console.ReadLine();
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(vehicleType);
+            vehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string numberOfWheels)
+        {
+            switch(numberOfWheels)
+            {
+                case "2":
+                    return new Motorcycle();
+                case "4":
+                    return new Car();
+                default:
+                    return new Vehicle();
+            }
+        }
+    }
+}
